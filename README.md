52b29bc5190e036cab9b80570e15dd33218a9203
liuxulei <liuxulei@gmail.com>
2015-03-06 11:05:06 +0800

    sync github

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# wfe-php
+#wfe-php
+因github不定时被墙，此版本仅作同步展示使用，日常更新版本请移步http://git.oschina.net/liuxulei/wfe-php
---
+++ application/controller/bookController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author liuxulei
+ * 2015-2-19
+ */
+
+class bookController extends Controller_Action {
+    
+    public function index(){
+        $param = $this->get_var('param', true, 'base');
+        $file = File_Directory::get_file('/files/book', $param, '.html');
+        if(is_file($file) === false){
+            $contents = '暂时无此内容';
+        }else{
+            $contents = File_Operation::read($file);
+        }
+        
+        $data = array('contents' => $contents);
+        $this->view($data);
+    }
+}
